ae718140855950d1f78646b9cc7f6491a0036337
hthegoat <harrisonkug@protonmail.com>
2021-02-09 18:30:08 -0500

    check it

--- docs/.vuepress/components/WorkWithMe.vue
+++ docs/.vuepress/components/WorkWithMe.vue
@@ -1,19 +1,66 @@
 <template>
-  <div class="md-card" :class="{ 'show-border': !hideBorder}">
-    <div v-if="image" class="card-image">
-      <img :src="$withBase(image)" alt="">
+ <div class="vuepress-plugin-contact-us-wrapper">
+        <div id="the-floating-button" @click="formVisible = !formVisible">
+            <font-awesome-icon :icon="appIcon" :size="size" :style="attachStyles()" />
+        </div>
+        <div class="contact-us-form-wrap" v-show="formVisible">
+            <div class="line-row">
+                <h2>{{ language.LB_FORM_TITLE }}</h2>
+            </div>
+            <ul class="error-box" v-if="errors.length > 0">
+                <li v-for="msg in errors" class="error-msg">{{ msg }}</li>
+            </ul>
+            <div class="line-row">
+                <input id="v-c-name" class="v-c-input" v-model="userFullName" :placeholder="language.LB_YOUR_NAME">
+            </div>
+            <div class="line-row">
+                <input id="v-c-email" class="v-c-input" v-model="emailFrom" :placeholder="language.LB_YOUR_EMAIL">
+            </div>
+            <div class="line-row">
+                <textarea rows="5" id="v-c-message" class="v-c-input" :placeholder="language.LB_YOUR_MESSAGE" v-model="message"></textarea>
+            </div>
+            <div class="line-row">
+                <button class="btn btn-send" @click.prevent="submitForm()">
+                    <font-awesome-icon :icon="spinnerIcon" v-show="sendingData" spin/>
+                    <font-awesome-icon :icon="sendIcon" v-show="!sendingData" />
+                    {{ language.LB_YOUR_BUTTON_SEND }}
+                </button>
+                <button class="btn btn-cancel" @click.prevent="formVisible = false">
+                    <font-awesome-icon :icon="closeIcon" />
+                    {{ language.LB_YOUR_BUTTON_CANCAL }}
+                </button>
+            </div>
+        </div>
     </div>
-    <div class="card-content">
-      <form>Maybe theres a</form>
-      <slot></slot>
-    </div>
-  </div>
 </template>
 
 <script>
+import VuepressContactUsLang from './utils'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner, faEnvelope, faShareSquare, faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import axios from 'axios'
+
+
 export default {
   props: ['image', 'hideBorder']
 };
+  data() {
+    return {
+      formVisible: false,
+            emailFrom: null,    // User's email
+            userFullName: null, // User's full name
+            message: null,      // User's message
+            emailTo: null,      // Receiver's email
+            lang: 'en',         // Language to use
+            sendgridApi: null,  // Sendgrid api key
+            errors: [],         // Validation
+            // UI
+            sendingData: false,     // Send message to API
+            size: '2x',         // Envolope Icon size
+            color: '#3eaf7c'        // Envolope Icon color
+    }
+  }
 </script>
 
 <style lang="stylus">
